87bb687daa1a230320d121044a721bcdc41d2718
Anand Kushwaha <98548977+ANandasaad@users.noreply.github.com>
2023-06-04 18:05:13 +0530

    express helper

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require("http");
 // const requestHandler = require('./routes/routes');
 // const assingmentHandler=require('./Assignment-1/assignmentroutes');
+const path=require('path');
 const express= require('express');
 const adminRoutes= require('./routes/admin');
 const shopRoutes=require("./routes/shop");
 const bodyParser = require("body-parser");
+const rootDir= require('./util/path');
 
 const app =express();
 app.use(bodyParser.urlencoded({extended:false}));
@@ -13,7 +15,8 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-  res.status(404).send("<h1>Page not found</h1>");
+  // res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(rootDir,'..','views','404.html'));
 })
 const server = http.createServer(app);
   // console.log(req.url, req.method,req.headers);
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,4 @@
-
+const path= require('path');
 const express= require('express');
 
 const router= express.Router();
@@ -6,7 +6,7 @@ const router= express.Router();
 
 router.get('/add-product',(req,res,next)=>{
     //  console.log('In the middleware') // middleware
-     res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button>add product</button> </form>')
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
    })
   
    router.post('/add-product',(req,res,next)=>{
